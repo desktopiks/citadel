debdb15251d4a0d9ae3479b0144bfeb1ef99f648
Arrgentum <alexeyvotintsev@yandex.ru>
2022-09-20 08:58:17 +0300

    ## What added: - Change size and coordinate window
    # Issue (#16)

--- interface/interface.py
+++ interface/interface.py
@@ -2,32 +2,27 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 
 
-class Example(QWidget):
+class Window(QWidget):
 
     def __init__(self):
         super().__init__()
 
-        self.initUI()  #Drawing the interface to method InitUi
-
-    def initUI(self):
-        self.desktop = QApplication.desktop()
-
-        #Get the screen sizes 
-        self.screenRect = self.desktop.screenGeometry()
-        self.height = self.screenRect.height()
-        self.width = self.screenRect.width()
-
-        #Show the window
-        self.show()
+        screen_rect = QApplication.desktop().screenGeometry()
+        screen_height, screen_width = screen_rect.height(), screen_rect.width()
+        window_height, window_width = int(screen_height / 9) * 5, int(screen_width / 7) * 3  # move window size
+        coordinate_window_height, coordinate_window_width = int(screen_height / 9) * 2, int(screen_width / 7) * 2 # move window coordinate
+        self.move(coordinate_window_width, coordinate_window_height) # change window coordinate
+        self.resize(window_width, window_height) # change window size
+        self.show() # show window
 
 
-def Application():
+def application():
     app = QApplication(sys.argv)
-    ex = Example()
-    ex.setWindowTitle('Citadels')
+    game_window = Window()
+    game_window.setWindowTitle('Citadels')
     sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
-    #Creating apps and objects
-    Application()
+    # Creating apps and objects
+    application()
