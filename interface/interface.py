4736d77a93d6063b8eafa8040b84aeefa6b11beb
Arrgentum <alexeyvotintsev@yandex.ru>
2022-09-20 15:00:44 +0300

    ## What added: - Add Geometry of the Game Window

--- interface/interface.py
+++ interface/interface.py
@@ -9,8 +9,8 @@ class Window(QWidget):
 
         screen_rect = QApplication.desktop().screenGeometry()
         screen_height, screen_width = screen_rect.height(), screen_rect.width()
-        window_height, window_width = int(screen_height / 9) * 5, int(screen_width / 7) * 3  # move window size
-        coordinate_window_height, coordinate_window_width = int(screen_height / 9) * 2, int(screen_width / 7) * 2 # move window coordinate
+        window_height, window_width = int(screen_height / 8) * 4, int(screen_width / 7) * 3  # move window size
+        coordinate_window_height, coordinate_window_width = int(screen_height / 8) * 2, int(screen_width / 7) * 2 # move window coordinate
         self.move(coordinate_window_width, coordinate_window_height) # change window coordinate
         self.resize(window_width, window_height) # change window size
         self.show() # show window
